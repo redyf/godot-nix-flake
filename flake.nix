bff08585ea85d05f8c8e9710932c6bd9fc87335b
redyf <mateusalvespereira7@gmail.com>
2025-11-07 09:53:05 -0300

    feat: add godot mono dev build

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Godot Engine dev builds";
+  description = "Godot Engine dev builds (Standard & Mono)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,78 +16,107 @@
           version,
           flavor,
           sha256,
+          mono ? false,
         }:
         let
-          runtimeLibs = with pkgs; [
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXinerama
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libXext
+          suffix = if mono then "-mono" else "";
+          slug = if mono then "mono_linux_x86_64" else "linux.x86_64";
 
-            libGL
+          runtimeLibs =
+            with pkgs;
+            [
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXinerama
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libXext
+              libGL
+              alsa-lib
+              libpulseaudio
+              wayland
+              systemd
+              dbus
+              fontconfig
+              freetype
+              zlib
+              stdenv.cc.cc.lib
+              libxkbcommon
+            ]
+            ++ pkgs.lib.optionals mono [
+              dotnet-sdk_8
+              dotnet-runtime_8
+            ];
 
-            alsa-lib
-            libpulseaudio
-
-            wayland
-
-            systemd
-            dbus
-            fontconfig
-            freetype
-            zlib
-            stdenv.cc.cc.lib
-          ];
         in
         pkgs.stdenv.mkDerivation {
-          pname = "godot-dev";
+          pname = "godot${suffix}";
           inherit version;
 
-          src = pkgs.fetchzip {
-            url = "https://downloads.godotengine.org/?version=${version}&flavor=${flavor}&slug=linux.x86_64.zip&platform=linux.64";
+          src = pkgs.fetchurl {
+            url = "https://downloads.godotengine.org/?version=${version}&flavor=${flavor}&slug=${slug}.zip&platform=linux.64";
             inherit sha256;
-            stripRoot = false;
-            extension = "zip";
+            name = "godot${suffix}.zip";
           };
 
           nativeBuildInputs = with pkgs; [
+            unzip
             autoPatchelfHook
             makeWrapper
           ];
 
           buildInputs = runtimeLibs;
 
+          unpackPhase = ''
+            mkdir src
+            cd src
+            unzip $src
+          '';
+
+          configurePhase = ''
+            export DOTNET_ROOT="${pkgs.dotnet-sdk_8}"
+            export PATH="${pkgs.dotnet-sdk_8}/bin:$PATH"
+          '';
+
           installPhase = ''
             mkdir -p $out/bin $out/lib
             cp -r ./* $out/lib/
 
-            godot_bin=$(find $out/lib -name "Godot_*" -type f -executable | head -n 1)
+            godot_bin=$(find $out/lib -type f -name "Godot_*" | head -n 1)
 
-            if [ -n "$godot_bin" ]; then
-              makeWrapper "$godot_bin" $out/bin/godot \
-                --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}"
-            fi
+            makeWrapper "$godot_bin" $out/bin/godot${suffix} \
+              --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}" \
+              --prefix PATH : "${pkgs.lib.makeBinPath (pkgs.lib.optionals mono [ pkgs.dotnet-sdk_8 ])}" \
+              --set DOTNET_ROOT "${pkgs.dotnet-sdk_8}"
           '';
 
-          meta = with pkgs.lib; {
-            description = "Godot Engine ${version} ${flavor}";
+          meta = {
+            description = "Godot Engine ${version} ${flavor}${suffix}";
             homepage = "https://godotengine.org";
             platforms = [ "x86_64-linux" ];
           };
         };
 
-    in
-    {
-      packages.${system} = {
-        godot-4_6-dev3 = mkGodotDev {
+      godotVersions = {
+        "4_6-dev3" = {
           version = "4.6";
           flavor = "dev3";
-          sha256 = "sha256-Fgvlt6TaKX/5N9GO6mGqYTMYjaBzME7Hs9CUEaboPyk=";
+          sha256 = "sha256-2GYLUkW/5UBR4UD3x/gO6zFweW2VlCywuGtFDw2/Wes=";
+          mono = false;
         };
 
-        default = self.packages.${system}.godot-4_6-dev3;
+        "4_6-dev3-mono" = {
+          version = "4.6";
+          flavor = "dev3";
+          sha256 = "sha256-F4uyzbgaxw+ZUEKEEw6sYABd6aW0dPEDz+ANvpe02AE=";
+          mono = true;
+        };
+      };
+
+    in
+    {
+      packages.${system} = (builtins.mapAttrs (_: args: mkGodotDev args) godotVersions) // {
+        default = self.packages.${system}."4_6-dev3";
       };
     };
 }
